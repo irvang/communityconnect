626a862c7200be56b8be4cf469bdb4edd3439214
Irving Angulo <irvang.manage@gmail.com>
2018-09-04 20:16:21 -0400

    Formatting files.

--- src/components/Map.js
+++ src/components/Map.js
@@ -10,68 +10,68 @@ const googleMapKey = 'AIzaSyAwKdrqS2GfCt9b2K1wAopDc9Ga0N1BVUM';
 const googleMapURL = `https://maps.googleapis.com/maps/api/js?key=${googleMapKey}&v=3.exp&libraries=geometry,drawing,places`;
 
 const organizationLayout = {
-  'icon-image': 'nature'
+	'icon-image': 'nature'
 };
 
 const mapStyle = {
-  position: 'absolute',
-  top: 0,
-  bottom: 0,
-  left: 0,
-  right: 0
+	position: 'absolute',
+	top: 0,
+	bottom: 0,
+	left: 0,
+	right: 0
 };
 
 export const Map = withScriptjs(withGoogleMap(props => (
-  <GoogleMap
-    ref={props.onMapLoad}
-    {...props}
-  >
-    <MarkerClusterer
-        averageCenter={true}
-        enableRetinaIcons={true}
-        gridSize={60}
-    >
-      { 
-        props.organizations.filter(org => org.coordinates).map(org =>
-          <OrganizationMarker
-            key={org.id}
-            organization={org}
-            open={org.showInfo}
-          />
-        )
-      }
-    </MarkerClusterer>
-  </GoogleMap>
+	<GoogleMap
+		ref={props.onMapLoad}
+		{...props}
+	>
+		<MarkerClusterer
+			averageCenter={true}
+			enableRetinaIcons={true}
+			gridSize={60}
+		>
+			{
+				props.organizations.filter(org => org.coordinates).map(org =>
+					<OrganizationMarker
+						key={org.id}
+						organization={org}
+						open={org.showInfo}
+					/>
+				)
+			}
+		</MarkerClusterer>
+	</GoogleMap>
 )));
 
 class OrganizationMap extends Component {
-  markerHover = (key, event) => {
-    event.map.getCanvas().style.cursor = 'pointer';
-    this.props.onMouseEnter(key);
-  }
+	markerHover = (key, event) => {
+		event.map.getCanvas().style.cursor = 'pointer';
+		this.props.onMouseEnter(key);
+	}
 
-  markerEndHover = (key, event) => {
-    event.map.getCanvas().style.cursor = '';
-    this.props.onMouseLeave();
-  }
+	markerEndHover = (key, event) => {
+		event.map.getCanvas().style.cursor = '';
+		this.props.onMouseLeave();
+	}
 
-  onOrganizationClick = (key, event) => {
-    this.props.onOrganizationClick(key);
-  }
+	onOrganizationClick = (key, event) => {
+		this.props.onOrganizationClick(key);
+	}
 
-  render() {
-      return (
-        <Map
-          googleMapURL={googleMapURL}
-          containerElement={ <div style={{ height: '100%' }} /> }
-          mapElement={ <div style={{ height: '100%' }} /> }
-          loadingElement={<div style={{ height: `100%` }} />}
-          zoom={this.props.zoom}
-          center={this.props.center}
-          organizations={this.props.organizations}
-        />
-      );
-  }
+	render() {
+		return (
+			<Map
+				googleMapURL={googleMapURL}
+				containerElement={<div style={{ height: '100%' }} />}
+				mapElement={<div style={{ height: '100%' }} />}
+				loadingElement={<div style={{ height: `100%` }} />}
+				zoom={this.props.zoom}
+				center={this.props.center}
+				organizations={this.props.organizations}
+			/>
+		);
+	}
 }
 
 export default OrganizationMap;
--- src/components/OrganizationMarker.js
+++ src/components/OrganizationMarker.js
@@ -2,59 +2,59 @@ import React, { Component } from 'react';
 import { Marker, InfoWindow } from 'react-google-maps';
 
 export class OrganizationMarker extends Component {
-  state = {
-    open: false,
-  }
+	state = {
+		open: false,
+	}
 
-  constructor(props) {
-    super(props);
+	constructor(props) {
+		super(props);
 
-    if (props.open) {
-      this.state.open = props.open;
-    }
-  }
+		if (props.open) {
+			this.state.open = props.open;
+		}
+	}
 
-  componentWillReceiveProps(nextProps) {
-    if (nextProps.open !== this.state.open) {
-      this.setState({
-        open: nextProps.open,
-      });
-    }
-  }
+	componentWillReceiveProps(nextProps) {
+		if (nextProps.open !== this.state.open) {
+			this.setState({
+				open: nextProps.open,
+			});
+		}
+	}
 
-  componentDidUpdate() {
-    const { open } = this.state;
-  }
+	componentDidUpdate() {
+		const { open } = this.state;
+	}
 
-  handleClick = (e) => {
-    this.setState({ open: true });
-  }
+	handleClick = (e) => {
+		this.setState({ open: true });
+	}
 
-  handleClose = () => {
-    this.setState({ open: false });
-  }
+	handleClose = () => {
+		this.setState({ open: false });
+	}
 
-  render() {
-    const { organization } = this.props;
+	render() {
+		const { organization } = this.props;
 
-    return (
-      <Marker
-        optimize={false}
-        position={organization.coordinates}
-        onClick={this.handleClick}
-      >
-        {this.state.open &&
-          <InfoWindow onCloseClick={this.handleClose}>
-            <div>
-              <h3>{organization.name}</h3>
-              <div>{organization.location}</div>
-              <div><a href={`tel:${organization.phone}`}>{organization.phone}</a></div>
-            </div>
-          </InfoWindow>
-        }
-      </Marker>
-    );
-  }
+		return (
+			<Marker
+				optimize={false}
+				position={organization.coordinates}
+				onClick={this.handleClick}
+			>
+				{this.state.open &&
+					<InfoWindow onCloseClick={this.handleClose}>
+						<div>
+							<h3>{organization.name}</h3>
+							<div>{organization.location}</div>
+							<div><a href={`tel:${organization.phone}`}>{organization.phone}</a></div>
+						</div>
+					</InfoWindow>
+				}
+			</Marker>
+		);
+	}
 }
 
 export default OrganizationMarker;
